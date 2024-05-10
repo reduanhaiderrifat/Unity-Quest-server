fad2fb46f53f3b3e206839513a14f00c9be9a347
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-05-10 20:33:37 +0600

    set up deete method for delete user's own post

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
 
     console.log(
