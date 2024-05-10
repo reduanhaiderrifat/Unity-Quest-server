96d887a789811cc8097685bd7610bb562d10b15f
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-05-10 15:59:28 +0600

    setup dynamic by id get one single data

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //midlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9zrvau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,6 +23,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    const volunteerCollection = client
+      .db("volunteerDB")
+      .collection("volunteerPost");
+    const beVolunteerCollection = client
+      .db("volunteerDB")
+      .collection("beVolunteer");
+
+    app.get("/allPost", async (req, res) => {
+      const result = await volunteerCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/singlePost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/post", async (req, res) => {
+      const postData = req.body;
+      console.log(postData);
+      const result = await volunteerCollection.insertOne(postData);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
 
